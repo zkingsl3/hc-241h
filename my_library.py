088d57be8a42e1f1c63ac996c4a0f5b0c3ef5c5f
zkingsl3 <130502314+zkingsl3@users.noreply.github.com>
2023-05-30 13:17:04 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -36,7 +36,7 @@ def metrics(parameter):
   assert isinstance(parameter, list), f'Expected parameter to be of type list but is instead {type(parameter)}'
   assert all(isinstance(value, list) for value in parameter), f'Your parameter is supposed to be a list of lists, but is not!'
   assert all(len(value) == 2 for value in parameter), f'Your list is not zipped- please zip it.'
-  assert all(isinstance(value[0], int) and isinstance(value[1], int) or isinstance(value[0], float) and isinstance(value[1], float) for value in parameter), f'Each value in pair should be an integer'
+  # assert all(isinstance(value[0], int) and isinstance(value[1], int) for value in parameter), f'Each value in pair should be an integer'
   assert all(value[0] >= 0 and value[1] >= 0 for value in parameter), f'Your values cannot be negative numbers'
 
   Accuracy = sum(p==a for p, a in parameter)/len(parameter)

10e3d9f8ed9d2c5c8db1b16914e11be51b6e9a89
zkingsl3 <130502314+zkingsl3@users.noreply.github.com>
2023-05-30 12:53:01 -0700

    Update my_library.py
    Added try_archs

--- my_library.py
+++ my_library.py
@@ -53,6 +53,25 @@ def generate_random(n):
   random_weights = [round(uniform(-1, 1), 2) for i in range(n)]
   return random_weights
 
+
+#I'll give you a start
+
+def try_archs(full_table, target, architectures, thresholds):
+  train_table, test_table = up_train_test_split(full_table, target, .4)
+  for arch in architectures:
+   all_results = up_neural_net(train_table, test_table, arch, target)
+  all_mets = []
+  for t in thresholds:
+    all_predictions = [1 if pos>=t else 0 for neg,pos in all_results]
+    pred_act_list = up_zip_lists(all_predictions, up_get_column(test_table, target))
+    mets = metrics(pred_act_list)
+    mets['Threshold'] = t
+    all_mets = all_mets + [mets]
+  print(f'Architecture: {arch}')
+  print(up_metrics_table(all_mets))
+  return None  #main use is to print out threshold tables, not return anything useful.
+
+
 def testing():
   return f'loaded!'
 
